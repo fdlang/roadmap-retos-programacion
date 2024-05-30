674ad95337070f231d99a36f978ede8e90d79c3d
Angel <purriaky@hotmail.com>
2024-05-30 18:23:49 +0100

    #00-python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/fdlang.py
@@ -0,0 +1,29 @@
+# https://www.python.org/
+
+'''
+Triples comillas 
+para comentarios 
+en multiples lineas
+'''
+
+"""
+Dobles comillas 
+para comentarios 
+en multiples lineas
+"""
+
+# Almohadilla para comentarios en una linea
+
+# En python no existen  constantes como tal, pero si ponemos en mayusculas el nombre de la varible se entiende por convenio que es una constante.
+CONSTANTE = 38
+
+# Tipos de datos primitivos
+n_int = 24 
+m_float = 24.5 
+boolean_true = True 
+boolean_false = False
+nulo = None
+text = 'Cadena de texto (str)'
+
+# Impresion por consola
+print("Hola, python")
