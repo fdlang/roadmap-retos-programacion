edba867517ab27fb2b0993e2bb1a5ed579c26beb
Angel <purriaky@hotmail.com>
2024-05-30 19:06:40 +0100

    #00-java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/fdlang.java
@@ -0,0 +1,34 @@
+// https://www.java.com/es/
+
+public class fdlang {
+
+    // Esto es un comentario de una linea 
+
+    /* 
+     * Cometarios
+     * en 
+     * varias lineas
+     */
+
+    static final String my_const = "esto es una constante";
+
+    public static void main(String[] args){
+
+        // Tipos de datos primitivos con valores maximo para cada tipo.
+        byte n_byte = 127;
+        short n_short = 32767;
+        int n_entero = 2147483647;
+        long n_long = 9223372036854775807L;
+        float n_float = 3.14f;
+        double n_double = 12.14159265359;
+        char n_char = 'a';
+        String text = "texto string";
+        boolean verdadero = true;
+        boolean falso = false;
+
+        // imprime texto por consola
+        System.out.println("Hola, Java !!");
+
+    }
+
+}
